096cbd6d521ced9281577520c7715df44788f731
sassihamdi-CD <hamdysassy7@gmail.com>
2023-03-16 22:31:52 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-void main(){
+void main(void){
 	
-	puts("Programming is like building a multilingual puzzlee\n");
-
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return 0;	
 }
