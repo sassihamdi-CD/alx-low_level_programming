aef732596b952f82b2bfb88457c889e741decf6d
sassihamdi-CD <hamdysassy7@gmail.com>
2023-03-18 00:24:57 +0100

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/* begining of the program and main function */
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+            putchar(abc);
+	}
+
+	putchar('\n');
+
+	return (o);
+}
+
