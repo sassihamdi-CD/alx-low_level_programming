2721a50a3df3390e76cdf4e9e98cf60c509cf737
sassihamdi-CD <hamdysassy7@gmail.com>
2023-03-21 22:55:39 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,27 +1,17 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_times_table - prints the n times table, starting with 0.
- * @n: the table limit (must be between 0 and 15).
- *
- * Return: void.
+ * main - computes and prints the sum of all the multiples of 3 or 5 below 1024
+ * Return: 0 if succesful
  */
-void print_times_table(int n)
+
+int main(void)
 {
-	int i, j;
+	int sum, i;
 
-	if (n < 0 || n > 15)
-		return;
-
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-			if (j == 0)
-				_putchar('0');
-			else
-				_printf(", %d", i * j);
-		}
-		_putchar('\n');
-	}
+	for (i = 1023, sum = 0; i; i--)
+		if (!(i % 3) || !(i % 5))
+			sum += i;
+	printf("%d\n", sum);
+	return (0);
 }
