b14bbe772f7e18bf430190052ac81113bbe93a7c
sassihamdi-CD <hamdysassy7@gmail.com>
2023-03-23 12:50:43 +0100

    main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+ #ifndef main_H_
+  2 #define main_H_
+  3 
+  4 int _putchar(char c);
+  5 void print_alphabet(void);
+  6 void print_alphabet_x10(void);
+  7 int _islower(int c);
+  8 int _isalpha(int c);
+  9 int print_sign(int n);
+ 10 int _abs(int);
+ 11 int print_last_digit(int);
+ 12 void jack_bauer(void);
+ 13 int add(int, int);
+ 14 void print_to_98(int n);
+ 15 void times_table(void);
+ 16 void print_times_table(int n);
+ 17 
+ 18 #endif
