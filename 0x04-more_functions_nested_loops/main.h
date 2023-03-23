cbc9318da7c8c399d1e71d4846d53a6842ef6ed5
sassihamdi-CD <hamdysassy7@gmail.com>
2023-03-23 12:53:12 +0100

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,18 +1,18 @@
- #ifndef main_H_
-  2 #define main_H_
-  3 
-  4 int _putchar(char c);
-  5 void print_alphabet(void);
-  6 void print_alphabet_x10(void);
-  7 int _islower(int c);
-  8 int _isalpha(int c);
-  9 int print_sign(int n);
- 10 int _abs(int);
- 11 int print_last_digit(int);
- 12 void jack_bauer(void);
- 13 int add(int, int);
- 14 void print_to_98(int n);
- 15 void times_table(void);
- 16 void print_times_table(int n);
- 17 
- 18 #endif
+#ifndef main_H
+#define main_H
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int n);
+void print_number(int n);
+int pattern(int n);
+
+#endif /* main_H */
