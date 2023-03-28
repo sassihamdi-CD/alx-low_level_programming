5660b1e0a8d6ccf971b68de37565ef2dd33eae5f
sassihamdi-CD <hamdysassy7@gmail.com>
2023-03-28 14:15:47 +0100

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swap to value pointers
+ * @a: first variable to swap
+ * @b: second variable to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+	
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
