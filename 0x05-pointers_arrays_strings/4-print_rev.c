bde4e57da0ea29a8e817b009d67cfff1dce6244b
sassihamdi-CD <hamdysassy7@gmail.com>
2023-03-28 14:43:14 +0100

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#inculde "main.h"
+
+/**
+ * print_rev - print a string in reverse followed by a new line
+ * @s: string to be reversed
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	i++;
+	i--;
+	while (i >= 0)
+	{
+	_putchar(s[i]);
+	i--;
+	}
+	_putchar('\n');
+}
