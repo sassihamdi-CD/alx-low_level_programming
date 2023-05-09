01b9572f5fddf1afb47aca0655fc55cdb325f010
sassihamdi-CD <hamdysassy7@gmail.com>
2023-05-09 01:13:10 +0100

    task done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,38 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * create_file - creates a file or truncates it
-  * @filename: the name of the file to create
-  * @text_content: NULL terminated string to write to file
-  * Return: 1 on succes, -1 on failue
-  */
-
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int file, len = 0, check = 0;
+	int fd;
+	int nletters;
+	int rwr;
 
 	if (!filename)
 		return (-1);
 
-	/* creates file or truncates a file, and give it permission -rw------ */
-	file = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
-	if (file == -1)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
 		return (-1);
 
-	/* if text, writes text into file */
-	if (text_content)
-	{
-		for (len = 0; text_content[len]; len++)
-			;
-		check = write(file, text_content, len);
-	}
-	/* closes file */
-	close(file);
-	if (check != len)
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 		return (-1);
+
+	close(fd);
 
 	return (1);
 }
