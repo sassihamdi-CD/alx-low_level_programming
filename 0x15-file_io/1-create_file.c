791f8d9a2b6acb5b4b1670d0badfe3dd31fc4bdd
sassihamdi-CD <hamdysassy7@gmail.com>
2023-05-09 01:11:22 +0100

    task done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,35 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: filename.
- * @text_content: added content.
- *
- * Return: 1 if the file exists. -1 if the fails does not exist
- * or if it fails.
- */
-int append_text_to_file(const char *filename, char *text_content)
+  * create_file - creates a file or truncates it
+  * @filename: the name of the file to create
+  * @text_content: NULL terminated string to write to file
+  * Return: 1 on succes, -1 on failue
+  */
+
+int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int file, len = 0, check = 0;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-
-	if (fd == -1)
+	/* creates file or truncates a file, and give it permission -rw------ */
+	file = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+	if (file == -1)
 		return (-1);
 
+	/* if text, writes text into file */
 	if (text_content)
 	{
-		for (nletters = 0; text_content[nletters]; nletters++)
+		for (len = 0; text_content[len]; len++)
 			;
-
-		rwr = write(fd, text_content, nletters);
-
-		if (rwr == -1)
-			return (-1);
+		check = write(file, text_content, len);
 	}
-
-	close(fd);
+	/* closes file */
+	close(file);
+	if (check != len)
+		return (-1);
 
 	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,5 +10,8 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_textfile(char *filename, char *filecopy, int letters);
+int create_copyfile(const char *filename, char *text_content);
+int append_text_to_copyfile(const char *filename, char *text_content);
 
-#endif /* end of _MAIN_H_ */
+#endif 
